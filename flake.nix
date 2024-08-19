e39bc7977a8582ce2dc64bd396e0e44909bb7fba
Xe Iaso <me@xeiaso.net>
2024-08-19 11:15:05 -0400

    add docker image to flake
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- flake.nix
+++ flake.nix
@@ -7,177 +7,188 @@
   };
 
   outputs = { self, nixpkgs, utils }:
-    utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ] (system:
-      let pkgs = import nixpkgs { inherit system; };
-          mkWeights = x: {
-            shape = x;
-            menu = x;
-            css = x;
-          };
-          weights = {
-            thin = mkWeights 100;
-            extralight = mkWeights 200;
-            light = mkWeights 300;
-            normal = mkWeights 400;
-            medium = mkWeights 500;
-            semibold = mkWeights 600;
-            bold = mkWeights 700;
-            extrabold = mkWeights 800;
-            black = mkWeights 900;
-          };
+    utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ]
+    (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        mkWeights = x: {
+          shape = x;
+          menu = x;
+          css = x;
+        };
+        weights = {
+          thin = mkWeights 100;
+          extralight = mkWeights 200;
+          light = mkWeights 300;
+          normal = mkWeights 400;
+          medium = mkWeights 500;
+          semibold = mkWeights 600;
+          bold = mkWeights 700;
+          extrabold = mkWeights 800;
+          black = mkWeights 900;
+        };
       in {
-        packages.default = pkgs.stdenvNoCC.mkDerivation {
-          name = "iosevka-iaso";
-          dontUnpack = true;
-          buildInputs = with pkgs; [ python311Packages.brotli python311Packages.fonttools ];
-          buildPhase = let
-            metric-override = {
-              cap = 790;
-              ascender = 790;
-              xHeight = 570;
-            };
-            iosevka-curly = pkgs.iosevka.override {
-              set = "curly";
-              privateBuildPlan = {
-                family = "Iosevka Curly Iaso";
-                spacing = "term";
-                serifs = "sans";
-                no-ligation = false;
-                ligations = { "inherit" = "default-calt"; };
-                no-cv-ss = true;
-                variants = {
-                  inherits = "ss01";
-                  design = {
-                    tilde = "low";
-                    number-sign = "slanted-tall";
-                    at = "fourfold-solid-inner-tall";
-                  };
-                };
-                weights.normal = mkWeights 400;
-                slopes.upright = {
-                  angle = 0;
-                  shape = "upright";
-                  menu = "upright";
-                  css = "normal";
-                };
-                widths.normal = {
-                  shape = 500;
-                  menu = 7;
-                  css = "normal";
-                };
-                inherit metric-override;
+        packages = rec {
+          default = pkgs.stdenvNoCC.mkDerivation {
+            name = "iosevka-iaso";
+            dontUnpack = true;
+            buildInputs = with pkgs; [
+              python311Packages.brotli
+              python311Packages.fonttools
+            ];
+            buildPhase = let
+              metric-override = {
+                cap = 790;
+                ascender = 790;
+                xHeight = 570;
               };
-            };
-            iosevka-aile = pkgs.iosevka.override {
-              set = "aile";
-              privateBuildPlan = {
-                family = "Iosevka Aile Iaso";
-                spacing = "quasi-proportional";
-                no-ligation = true;
-                no-cv-ss = true;
-                variants = {
-                  inherits = "ss01";
-                  design = {
-                    tilde = "low";
-                    number-sign = "slanted-tall";
-                    at = "fourfold-solid-inner-tall";
+              iosevka-curly = pkgs.iosevka.override {
+                set = "curly";
+                privateBuildPlan = {
+                  family = "Iosevka Curly Iaso";
+                  spacing = "term";
+                  serifs = "sans";
+                  no-ligation = false;
+                  ligations = { "inherit" = "default-calt"; };
+                  no-cv-ss = true;
+                  variants = {
+                    inherits = "ss01";
+                    design = {
+                      tilde = "low";
+                      number-sign = "slanted-tall";
+                      at = "fourfold-solid-inner-tall";
+                    };
                   };
-                };
-                slopes = {
-                  upright = {
+                  weights.normal = mkWeights 400;
+                  slopes.upright = {
                     angle = 0;
                     shape = "upright";
                     menu = "upright";
                     css = "normal";
                   };
-                  italic = {
-                    angle = 9.4;
-                    shape = "italic";
-                    menu = "italic";
-                    css = "italic";
-                  };
-                };
-                inherit metric-override weights;
-              };
-            };
-            iosevka-etoile = pkgs.iosevka.override {
-              set = "etoile";
-              privateBuildPlan = {
-                family = "Iosevka Etoile Iaso";
-                spacing = "quasi-proportional";
-                serifs = "slab";
-                no-ligation = true;
-                no-cv-ss = true;
-                variants = {
-                  inherits = "ss01";
-                  design = {
-                    f = "flat-hook-serifed";
-                    j = "flat-hook-serifed";
-                    t = "flat-hook";
-                    capital-t = "serifed"; # not part of original Iosevka Aile
-                    #capital-g = "toothless-rounded-serifless-hooked";
-                    i = "zshaped";
-                    r = "serifed";
-
-                    tilde = "low";
-                    number-sign = "slanted-tall";
-                    at = "fourfold-solid-inner-tall";
-                  };
-                  italic = { f = "flat-hook-tailed"; };
-                };
-                slopes = {
-                  upright = {
-                    angle = 0;
-                    shape = "upright";
-                    menu = "upright";
+                  widths.normal = {
+                    shape = 500;
+                    menu = 7;
                     css = "normal";
                   };
-                  italic = {
-                    angle = 9.4;
-                    shape = "italic";
-                    menu = "italic";
-                    css = "italic";
-                  };
+                  inherit metric-override;
                 };
-                inherit metric-override weights;
               };
-            };
-          in ''
-            mkdir -p ttf
-            for ttf in ${iosevka-curly}/share/fonts/truetype/*.ttf ${iosevka-aile}/share/fonts/truetype/*.ttf ${iosevka-etoile}/share/fonts/truetype/*.ttf; do
-              cp $ttf .
+              iosevka-aile = pkgs.iosevka.override {
+                set = "aile";
+                privateBuildPlan = {
+                  family = "Iosevka Aile Iaso";
+                  spacing = "quasi-proportional";
+                  no-ligation = true;
+                  no-cv-ss = true;
+                  variants = {
+                    inherits = "ss01";
+                    design = {
+                      tilde = "low";
+                      number-sign = "slanted-tall";
+                      at = "fourfold-solid-inner-tall";
+                    };
+                  };
+                  slopes = {
+                    upright = {
+                      angle = 0;
+                      shape = "upright";
+                      menu = "upright";
+                      css = "normal";
+                    };
+                    italic = {
+                      angle = 9.4;
+                      shape = "italic";
+                      menu = "italic";
+                      css = "italic";
+                    };
+                  };
+                  inherit metric-override weights;
+                };
+              };
+              iosevka-etoile = pkgs.iosevka.override {
+                set = "etoile";
+                privateBuildPlan = {
+                  family = "Iosevka Etoile Iaso";
+                  spacing = "quasi-proportional";
+                  serifs = "slab";
+                  no-ligation = true;
+                  no-cv-ss = true;
+                  variants = {
+                    inherits = "ss01";
+                    design = {
+                      f = "flat-hook-serifed";
+                      j = "flat-hook-serifed";
+                      t = "flat-hook";
+                      capital-t = "serifed"; # not part of original Iosevka Aile
+                      #capital-g = "toothless-rounded-serifless-hooked";
+                      i = "zshaped";
+                      r = "serifed";
 
-              echo "processing $ttf"
+                      tilde = "low";
+                      number-sign = "slanted-tall";
+                      at = "fourfold-solid-inner-tall";
+                    };
+                    italic = { f = "flat-hook-tailed"; };
+                  };
+                  slopes = {
+                    upright = {
+                      angle = 0;
+                      shape = "upright";
+                      menu = "upright";
+                      css = "normal";
+                    };
+                    italic = {
+                      angle = 9.4;
+                      shape = "italic";
+                      menu = "italic";
+                      css = "italic";
+                    };
+                  };
+                  inherit metric-override weights;
+                };
+              };
+            in ''
+              mkdir -p ttf
+              for ttf in ${iosevka-curly}/share/fonts/truetype/*.ttf ${iosevka-aile}/share/fonts/truetype/*.ttf ${iosevka-etoile}/share/fonts/truetype/*.ttf; do
+                cp $ttf .
 
-              name=`basename -s .ttf $ttf`
-              pyftsubset \
-                $ttf \
-                --output-file="$name".woff2 \
-                --flavor=woff2 \
-                --layout-features=* \
-                --no-hinting \
-                --desubroutinize \
-                --unicodes="U+0000-0170,U+00D7,U+00F7,U+2000-206F,U+2074,U+20AC,U+2122,U+2190-21BB,U+2212,U+2215,U+F8FF,U+FEFF,U+FFFD,U+00E8"
-              mv *.ttf ttf
-            done
+                echo "processing $ttf"
 
-            ${pkgs.zip}/bin/zip ttf.zip ttf/*.ttf
+                name=`basename -s .ttf $ttf`
+                pyftsubset \
+                  $ttf \
+                  --output-file="$name".woff2 \
+                  --flavor=woff2 \
+                  --layout-features=* \
+                  --no-hinting \
+                  --desubroutinize \
+                  --unicodes="U+0000-0170,U+00D7,U+00F7,U+2000-206F,U+2074,U+20AC,U+2122,U+2190-21BB,U+2212,U+2215,U+F8FF,U+FEFF,U+FFFD,U+00E8"
+                mv *.ttf ttf
+              done
 
-            ${pkgs.python3}/bin/python3 ${./src/mkcss.py}
-            ${pkgs.python3}/bin/python3 ${./src/specimen.py}
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp *.woff2 $out
-            cp ttf.zip $out
+              ${pkgs.zip}/bin/zip ttf.zip ttf/*.ttf
 
-            cp family.css $out/family.css
-            cp *.html $out
-          '';
+              ${pkgs.python3}/bin/python3 ${./src/mkcss.py}
+              ${pkgs.python3}/bin/python3 ${./src/specimen.py}
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp *.woff2 $out
+              cp ttf.zip $out
+
+              cp family.css $out/family.css
+              cp *.html $out
+            '';
+          };
+
+          docker = pkgs.dockerTools.buildLayeredImage {
+            name = "ghcr.io/xe/iosevka";
+            tag = "latest";
+            contents = with pkgs; [ default ];
+          };
         };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = [pkgs.python3];
-        };
+        devShells.default = pkgs.mkShell { buildInputs = [ pkgs.python3 ]; };
       });
 }
